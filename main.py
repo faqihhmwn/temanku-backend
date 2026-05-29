1c8f73e9d1a668cb09762288476cd0df675f9565
Muhammad Faqih Himawan <muhammadfaqihhmwn@gmail.com>
2026-05-29 17:01:03 +0700

    Revisi CORS middleware

--- main.py
+++ main.py
@@ -22,14 +22,19 @@ from routes import profile
 app = FastAPI()
 
 # CORS Middleware
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://temanku-frontend.vercel.app",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-os.makedirs("uploads", exist_ok=True)
 
 # Static files untuk upload gambar dictionary
 app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
