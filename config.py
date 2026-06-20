5ec07509adc38bb42ae036def8022aca039c5fda
Muhammad Faqih Himawan <muhammadfaqihhmwn@gmail.com>
2026-06-20 21:41:48 +0700

    Penambahan waktu expire token

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ print("=================================")
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM", "HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(
-    os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "30")
+    os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "120")
 )
 
 
