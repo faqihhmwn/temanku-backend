97c50af4938fa56dd85ccbf51f688c6574c3487a
Muhammad Faqih Himawan <muhammadfaqihhmwn@gmail.com>
2026-07-12 22:12:50 +0700

    Menurunkan treshold inferensi model

--- services/predict_service.py
+++ services/predict_service.py
@@ -8,7 +8,12 @@ model = YOLO(MODEL_PATH)
 
 
 def predict_image(image_path: str):
-    results = model(image_path,conf=0.5)
+    results = model(
+        image_path,
+        conf=0.35,
+        iou=0.45,
+        verbose=False
+    )
 
     predictions = []
 
@@ -23,13 +28,22 @@ def predict_image(image_path: str):
             }
 
         for box in boxes:
+
             class_id = int(box.cls[0])
             confidence = float(box.conf[0])
             class_name = model.names[class_id]
 
+            x1, y1, x2, y2 = box.xyxy[0].tolist()
+
             predictions.append({
                 "label": class_name,
-                "confidence": confidence
+                "confidence": confidence,
+                "bbox": {
+                    "x1": float(x1),
+                    "y1": float(y1),
+                    "x2": float(x2),
+                    "y2": float(y2)
+                }
             })
 
     if len(predictions) == 0:
@@ -44,17 +58,18 @@ def predict_image(image_path: str):
         key=lambda x: x["confidence"]
     )
 
-    # filter prediction
+    # # filter prediction
 
-    if best_prediction["confidence"] < 0.6:
-        return {
-            "prediction": None,
-            "confidence": best_prediction["confidence"],
-            "message": "Prediksi tidak stabil"
-        }
+    # if best_prediction["confidence"] < 0.45:
+    #     return {
+    #         "prediction": None,
+    #         "confidence": best_prediction["confidence"],
+    #         "message": "Prediksi tidak stabil"
+    #     }
 
     return {
         "prediction": best_prediction["label"],
-        "confidence": best_prediction["confidence"],
+        "confidence": round(best_prediction["confidence"], 3),
+        "bbox": best_prediction["bbox"],
         "all_predictions": predictions
     }
